9978ada577a8634b269215f5169f3b3aee617472
Kenobi-Knobs <konchatniyvitaliy@gmail.com>
2021-03-29 16:36:57 +0300

    Add google authentification

--- city_swipe/urls.py
+++ city_swipe/urls.py
@@ -2,8 +2,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def my_logout(request):
+    logout(request)
+    return redirect('/city_swipe_app/')
 
 urlpatterns = [
     url(r'^city_swipe_app/', include('city_swipe_app.urls')),
     url(r'^admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
+    path('logout/', my_logout, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- city_swipe_app/views.py
+++ city_swipe_app/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+#from .models import Card
 
 def index(request):
+    context = {
+        'cards': "WELCOME!"
+        if request.user.is_authenticated else []
+    }
     return render(request, "index.html")
 
 def instruction(request):
