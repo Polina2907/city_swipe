6f8dea5f9b8c7e193826082e39910aa6cf902364
Polina2907 <polina2907polina@gmail.com>
2021-04-18 23:20:07 +0300

    adding ManytoMany relation and sql request

--- city_swipe_app/urls.py
+++ city_swipe_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from .views import getCard
 
 app_name = 'city_swipe_app'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'), #r-regexp, name-название представления, views.index - место где определили урл
     path('instruction/', views.instruction, name='instruction'),
     path('mainPage/', views.mainPage, name='mainPage'),
+    path('getCard/', views.getCard, name='getCard'),
 ]
--- city_swipe_app/views.py
+++ city_swipe_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-#from .models import Card
+from .models import Card
 
 def index(request):
     if request.user.is_authenticated:
@@ -17,8 +17,10 @@ def mainPage(request):
     else:
         return render(request, "index.html")
 
-# def getCard(request):
-#     if request.user.is_authenticated:
-#         return render(request, "mainPage.html")
-#     else:
-#         return render(request, "index.html")
+def getCard(request):
+    if request.user.is_authenticated:
+        a = ''
+        for card in Card.objects.raw('SELECT * FROM Card'):
+            return card
+    else:
+        return render(request, "index.html")
